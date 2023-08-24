16ba5d0e50573059521978c205568ca39e37d296
Gileu Neto <gileuneto@gmail.com>
2023-08-24 14:22:41 -0300

    add ids

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,22 +7,18 @@
     <link href="http://localhost/encurtadorUrl/laravel/resources/css/app.css" rel="stylesheet">
 </head>
 <body>
-    <main class=".bg-secondary.bg-gradient">
-            
+    <main class=".bg-secondary.bg-gradient">        
         <div class="container_url ">
             <h1 class="h1">Encurtador</h1>
             <p>Seja bem vindo insira uma url</p>           
-            <input type="text" class="form-control" placeholder="https://www.exemple.com.br" >            
-            <div class="d-grid p-2 gap-2 col-6 mx-auto">
-                <button type="button" class="btn btn-dark">Encurtar</button>  
-                <button type="button" class="btn btn-success">Copiar</button>
-            </div>
+            <input id="link" type="text" class="form-control" placeholder="https://www.exemple.com.br" >            
+        <div class="d-grid p-2 gap-2 col-6 mx-auto">
+            <button id="encurtador" type="button" class="btn btn-dark">Encurtar</button>  
+            <button id="copiar" type="button" class="btn btn-success">Copiar</button>
         </div>
-    </main>     
-        
-    
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>    
-     
+        </div>
+    </main>       
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>     
 </body>
 </html>
