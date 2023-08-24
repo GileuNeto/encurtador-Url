d548aed222da672f96a9dd04d3098cea5c097104
Gileu Neto <gileuneto@gmail.com>
2023-08-24 14:16:06 -0300

    adicionando front

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encurtador URL</title>
+    <link href="http://localhost/encurtadorUrl/laravel/resources/css/app.css" rel="stylesheet">
+</head>
+<body>
+    <main class=".bg-secondary.bg-gradient">
+            
+        <div class="container_url ">
+            <h1 class="h1">Encurtador</h1>
+            <p>Seja bem vindo insira uma url</p>           
+            <input type="text" class="form-control" placeholder="https://www.exemple.com.br" >            
+            <div class="d-grid p-2 gap-2 col-6 mx-auto">
+                <button type="button" class="btn btn-dark">Encurtar</button>  
+                <button type="button" class="btn btn-success">Copiar</button>
+            </div>
+        </div>
+    </main>     
+        
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>    
+     
+</body>
+</html>
