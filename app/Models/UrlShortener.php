21975e2f9859c509853a351176a9cf0abc8cba05
Gileu Neto <gileuneto@gmail.com>
2023-08-27 15:17:47 -0300

    configurando o curl

--- app/Models/UrlShortener.php
+++ app/Models/UrlShortener.php
@@ -11,20 +11,28 @@ class UrlShortener{
             'url' => $url
         ];
 
-        $response = self::send($params);
+        $response = self::send($params); 
+    }
+
+    private static function send($params=[]){
+        $endpoint = 'https://ulvis.net/API/write/get?'.http_build_query($params);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl,[
+            CURLOPT_URL => $endpoint,
+            CURLOPT_RETURNTRANSFER => true, 
+            CURLOPT_CUSTOMREQUEST => 'GET'
+        ]);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
         echo "<pre>";
         print_r($response);
         echo "</pre>";exit;
 
-    }
 
-    private static function send($params=[]){
-        $endpoint = 'https://ulvis.net/API/write/get?'.http_build_query($params) ;
-
-        echo "<pre>";
-        print_r($endpoint);
-        echo "</pre>";exit;
-
-        $curl = curl_init();
     }
 }
